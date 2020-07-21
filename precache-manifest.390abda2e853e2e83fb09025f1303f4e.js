cdf3bfc5a816a1398bae8e709ee6440c16d9db5f
Ravikesh Yadav <ravikesh@netzary.com>
2020-07-21 17:30:22 +0530

    Updates

---
+++ precache-manifest.390abda2e853e2e83fb09025f1303f4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba96a1eb16a43ba0990103f9cae9f748",
+    "url": "/github-jobs/index.html"
+  },
+  {
+    "revision": "7841e651c1b34072caf5",
+    "url": "/github-jobs/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "e871e1cea5ebc1dd1afc",
+    "url": "/github-jobs/static/js/2.ace3c952.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/github-jobs/static/js/2.ace3c952.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7841e651c1b34072caf5",
+    "url": "/github-jobs/static/js/main.99710244.chunk.js"
+  },
+  {
+    "revision": "e8f8862f689bdacc5a41",
+    "url": "/github-jobs/static/js/runtime-main.92b0d155.js"
+  }
+]);
